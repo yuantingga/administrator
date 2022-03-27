35717bc4e4610dce83a8b739476721a8f118daa3
yuanttrerfd <1598538014@qq.com>
2022-03-27 22:14:18 +0800

    主分支错误修改子分支合并进行修改

--- src/components/DataStatistics/DataStatistics.vue
+++ src/components/DataStatistics/DataStatistics.vue
@@ -4,6 +4,7 @@
       <el-breadcrumb separator-class="el-icon-arrow-right">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
         <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+        <el-breadcrumb-item>数据报表</el-breadcrumb-item>
 
       </el-breadcrumb>
     </el-row>
