5c262bc73ad04d21d8253aca23315de1194cc47e
yuanttrerfd <1598538014@qq.com>
2022-12-07 22:56:08 +0800

    重新设置api地址

--- vue.config.js
+++ vue.config.js
@@ -36,25 +36,25 @@ module.exports = {
   }
 }
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-  devServer: {
-    headers: {
-      // 允许跨域的源url地址
-      'Access-Control-Allow-Origin': '*'
-    },
-    proxy: {
-      // 这里监听一个别名，下面替换掉
-      '/api': { // 这里最好有一个 /
-        target: 'https://www.liulongbin.top:8888/api/private/v1/', // 后台接口域名
-        // ws: true,
-        // 如果要代理 webSockets，配置这个参数
-        // secure: false,
-        // 如果是https接口，需要配置这个参数
-        changeOrigin: true, // 是否跨域
-        pathRewrite: {
-          '^/api': '' // 替换成
-        }
-      }
-    }
-  }
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  // devServer: {
+  //   headers: {
+  //     // 允许跨域的源url地址
+  //     'Access-Control-Allow-Origin': '*'
+  //   },
+  //   proxy: {
+  //     // 这里监听一个别名，下面替换掉
+  //     '/api': { // 这里最好有一个 /
+  //       target: 'https://www.liulongbin.top:8888/api/private/v1/', // 后台接口域名
+  //       // ws: true,
+  //       // 如果要代理 webSockets，配置这个参数
+  //       // secure: false,
+  //       // 如果是https接口，需要配置这个参数
+  //       changeOrigin: true, // 是否跨域
+  //       pathRewrite: {
+  //         '^/api': '' // 替换成
+  //       }
+  //     }
+  //   }
+  // }
 }
